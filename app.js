5334793ec3b72141066deab0df82e645dc7a41c5
chachamame <kimi.jack.of.all.trades@gmail.com>
2019-01-27 23:20:12 +0900

    Add animals

--- app.js
+++ app.js
@@ -3,6 +3,56 @@
 
   const click = document.getElementById('click');
   const messages = document.getElementsByClassName('messages');
+  var isClicked = false;
+
+  var contents = [
+    {
+    src: "https://1.bp.blogspot.com/-4N2T5W6jo_o/VCIiuUHNwEI/AAAAAAAAmeo/_lyIGo3afK4/s800/animal_hiyoko.png",
+    name: 'HIYOKO',
+    text:'大丈夫だよ、落ち着いてね'
+    },
+    {
+      src: "https://3.bp.blogspot.com/--IJXuiGRidY/WNir5IfLk3I/AAAAAAABC28/81L3mxcIe9EoqcK9uqCXbVxZ4YO36JzlQCLcB/s800/animal_mitsubachi.png",
+      name: 'ミツ・バチコ',
+      text:'はちみつでも食べてネ'
+    },
+    {
+      src: "https://2.bp.blogspot.com/-uLsKgsN9njE/VCIix1WrktI/AAAAAAAAmfU/SloY2fZacmo/s800/animal_usagi.png",
+      name: 'Mr.R',
+      text:"Don't Warry. Be happy"
+    },
+    {
+      src: "https://3.bp.blogspot.com/-n0PpkJL1BxE/VCIitXhWwpI/AAAAAAAAmfE/xLraJLXXrgk/s800/animal_hamster.png",
+      name: 'ひまわりの種大好き匿名希望',
+      text:'ひまわりの種を食べるでチュ'
+    },
+    {
+      src: "https://4.bp.blogspot.com/-7DLdBODmEqc/VCIitQRzAWI/AAAAAAAAmeY/g1fjm8NqyaI/s800/animal_arupaka.png",
+      name: 'あるぱかーき',
+      text: '私のもふもふでぎゅっとしてあげる～'
+    },
+    {
+      src: "https://2.bp.blogspot.com/-dvDN3SxnRWE/VCIivNhugVI/AAAAAAAAmew/sEC6XC1sGwk/s800/animal_kuma.png",
+      name: 'かつてクマと呼ばれていたもの',
+      text: '蜂蜜は全てを解決する。蜂蜜を食べるべし。'
+    },
+    {
+      src: "https://1.bp.blogspot.com/-LFh4mfdjPSQ/VCIiwe10YhI/AAAAAAAAme0/J5m8xVexqqM/s800/animal_neko.png",
+      name: 'Meow',
+      text: 'Meooww...memememe...meow!!'
+    },
+    {
+      src: "https://4.bp.blogspot.com/-CtY5GzX0imo/VCIixcXx6PI/AAAAAAAAmfY/AzH9OmbuHZQ/s800/animal_penguin.png",
+      name: 'PenG',
+      text: '群れのみんなで集まるといいと思うよ！'
+    },
+    {
+      src: "https://2.bp.blogspot.com/-81gQBAcgSEc/VCIi95CkyYI/AAAAAAAAmhw/z9DvwU2vvmM/s800/icon_others_08.png",
+      name: '象',
+      text: 'There is always light behind the clouds.'
+    },
+  ];
+
 
   function createContent( data_src , data_name , data_text ) {
     var message = document.createElement('div');
@@ -32,6 +82,10 @@
   }
 
   click.addEventListener('click', function (e) {
+    if (isClicked) {
+      return;
+    }
+    isClicked = true;
     const user = createContent("https://1.bp.blogspot.com/-FfjY4DibSI4/VCIiuxKtLRI/AAAAAAAAmes/40lCg_r9U2g/s800/animal_inu.png",
       "あなた", "つらい……");
     messages[0].appendChild(user);
@@ -46,28 +100,6 @@
     })();
   });
 
-  const contents = [
-    {
-    src: "https://1.bp.blogspot.com/-4N2T5W6jo_o/VCIiuUHNwEI/AAAAAAAAmeo/_lyIGo3afK4/s800/animal_hiyoko.png",
-    name: 'HIYOKO',
-    text:'大丈夫だよ、落ち着いてね'
-    },
-    {
-      src: "https://3.bp.blogspot.com/--IJXuiGRidY/WNir5IfLk3I/AAAAAAABC28/81L3mxcIe9EoqcK9uqCXbVxZ4YO36JzlQCLcB/s800/animal_mitsubachi.png",
-      name: 'ミツ・バチコ',
-      text:'はちみつでも食べてネ'
-    },
-    {
-      src: "https://2.bp.blogspot.com/-uLsKgsN9njE/VCIix1WrktI/AAAAAAAAmfU/SloY2fZacmo/s800/animal_usagi.png",
-      name: 'Mr.R',
-      text:"Don't Warry. Be happy"
-    },
-    {
-      src: "https://3.bp.blogspot.com/-n0PpkJL1BxE/VCIitXhWwpI/AAAAAAAAmfE/xLraJLXXrgk/s800/animal_hamster.png",
-      name: 'ひまわりの種大好き匿名希望',
-      text:'ひまわりの種を食べるでチュ'
-    }
-  ];
 
   function sleep(time) {
     return new Promise((resolve, reject) => {
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 **優しい世界です**
+デモ
+https://chachamame.github.io/YasasiiSekai/.
